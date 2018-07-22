9b4f6bc94a6e3d8b5f0059c4b3190d50632f3819
Helton Denis <heltondeniss@gmail.com>
2018-07-22 10:57:37 -0300

    Adiçao das despesas

--- src/AddDespesa.js
+++ src/AddDespesa.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList, Button} from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TextInput} from 'react-native';
 import firebase from './FirebaseConnection.js';
 
 export default class AddDespesa extends Component {
 
 	static navigationOptions = {
-		title:"Adicionar Despesas",
+		title:"Adicionar Despesa",
 		headerStyle:{
 			backgroundColor: '#6dd5ed'
 		},
@@ -15,30 +15,55 @@ export default class AddDespesa extends Component {
 	constructor(props) {
 	  super(props);
 	  this.state = {
-	  	receitas:{
-
-	  	}
+	  	valor: ''
 	  };
 
-	
+	this.add = this.add.bind(this);
 	}
 
 	/* Métodos */
-	addReceita(){
-		alert('Receita');
-	}
-	addDespesa(){
-		alert('Despesa');
-	}
-	sair(){
-		firebase.auth().signOut();
-	}
+	add(){
+		if (this.state.valor != '') {
 
+
+			let historico = firebase.database().ref('historico')
+					.child(firebase.auth().currentUser.uid);
+			let user = firebase.database().ref('users')
+				.child(firebase.auth().currentUser.uid);
+
+			/* Update no banco Histórico */
+			let key = historico.push().key;
+
+			historico.child(key).set({
+				type:'despesa',
+				valor:this.state.valor
+			});
+
+			/* Update no banco Saldo */
+				user.once('value').then((snapshot)=>{
+
+					let saldo = parseFloat(snapshot.val().saldo);
+					saldo -= parseFloat(this.state.valor);
+
+					user.set({
+						saldo:saldo
+					});
+
+					alert("Saldo atualizado");
+				});
+		}
+	}
 
 	render(){
 		return (
 			<View style={styles.container}>
-				<Text>...</Text>
+				<Text style={styles.lblDespesa}>Adicione uma Despesa:</Text>
+				<TextInput 
+				keyboardType="numeric" 
+				style={styles.btnAdd} 
+				onChangeText={(valor)=>this.setState({valor})} 
+				/>
+				<Button title="Despesa +" onPress={this.add}></Button>
 			</View>
 
 		);
@@ -49,6 +74,15 @@ const styles = StyleSheet.create({
 
 	container:{
 		flex:1
+	},
+
+	btnAdd:{
+		height:40,
+		marginTop:20
+	},
+
+	lblDespesa:{
+		marginTop:20
 	}
 
 
